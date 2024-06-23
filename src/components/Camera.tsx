ccd5a8fb25443cd7c9ec12b1782b1ab3614273f8
Brandon Li <byl23@cornell.edu>
2024-06-23 18:22:13 -0400

    removed unused import

--- src/components/Camera.tsx
+++ src/components/Camera.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef, useMemo } from "react";
+import React, { useEffect, useRef, useMemo } from "react";
 import * as THREE from "three";
 import type { ImageData, CameraData } from "../hooks/useCOLMAPLoader";
 import { useThree } from "@react-three/fiber";
