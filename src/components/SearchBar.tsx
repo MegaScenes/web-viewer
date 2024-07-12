73c76100ce41c2d5aa1f9f523d12c318ddb4179a
Brandon Li <byl23@cornell.edu>
2024-07-12 12:25:32 -0400

    remove unused dep

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo, useCallback } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import { useSearchParams } from "next/navigation";
 import Autocomplete from "@mui/material/Autocomplete";
 import TextField from "@mui/material/TextField";
